fe10f7d9a24eecba78c0e03b4b3d51478ae8f42a
Kevin Stevanus <kevinstevanus777@gmail.com>
2020-07-22 12:47:46 +0700

    npm init

---
+++ routes.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const requestHandler = (req,res) => {
+    const url = req.url;
+    const method = req.method;
+  if (url === "/") {
+    console.log("you are on rooot");
+  } else if (url === "/message" && method === "POST") {
+    console.log('you are on messages')
+  }
+};
+module.exports = requestHandler;
