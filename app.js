58d211285905fe63e94fee85e37c930053be5527
Kevin Stevanus <kevinstevanus777@gmail.com>
2020-07-23 00:03:17 +0700

    404 page

--- app.js
+++ app.js
@@ -7,5 +7,8 @@ const userRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(userRoutes);
+app.use((req, res, next) => {
+  res.status(404).send("<h1>page not found</h1>");
+});
 
 app.listen(3000);
