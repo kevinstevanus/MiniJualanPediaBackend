049b69671793be0769c387aae50575baf1ccfda2
Kevin Stevanus <kevinstevanus777@gmail.com>
2020-07-23 19:08:50 +0700

    adding simple controllers

---
+++ controllers/products.js
@@ -0,0 +1,7 @@
+exports.getAddProduct = (req, res, next) => {
+  console.log("get products");
+};
+
+exports.postAddProduct = (req, res, next) => {
+  console.log("post product");
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const productController = require("../controllers/products");
 
-router.get("/products", (req, res, next) => {
-  console.log("get products");
-});
+router.get("/products", productController.getAddProduct);
 
 router.post("/add-products", (req, res, next) => {
   console.log("POST PRODUCTS");
