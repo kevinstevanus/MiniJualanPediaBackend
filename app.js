da6397d36a7b79b8183e65fb90811b73a35552f1
Kevin Stevanus <kevinstevanus777@gmail.com>
2020-07-23 18:49:58 +0700

    filtering

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const adminRoutes = require("./routes/admin");
 const userRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(userRoutes);
 app.use((req, res, next) => {
   res.status(404).send("<h1>page not found</h1>");
